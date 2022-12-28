4b96ec15a5c063d88f247cf2eaea46b8e2b339a1
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2022-12-28 18:30:50 -0500

    fix: setup for hosting on VPS

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import cspDirectives from './csp-directives.mjs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			mode: 'hash',
+			directives: cspDirectives
+		}
 	}
 };
 
